56164372d47058932e9f3c4fc81d5c79f677abdc
wuwj-cn <wenjie.0617@gmail.com>
2017-09-18 18:46:00 +0800

    修改CDM，编码规则表增加当前值字段

--- src/test/java/com/mplus/utils/RuleUtilTest.java
+++ src/test/java/com/mplus/utils/RuleUtilTest.java
@@ -1,8 +1,7 @@
 package com.mplus.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
