c2302267a3081f6fd6a3737310b06095519d0ae0
wuwj-cn <wenjie.0617@gmail.com>
2018-04-17 13:07:51 +0800

    设置swagger可匿名访问

--- src/main/java/com/mplus/config/ShiroConfig.java
+++ src/main/java/com/mplus/config/ShiroConfig.java
@@ -60,6 +60,7 @@ public class ShiroConfig {
 		filterChainDefinitionMap.put("/api/", "anon");
 		filterChainDefinitionMap.put("/api/login", "anon");
 		filterChainDefinitionMap.put("/api/index", "anon");
+		filterChainDefinitionMap.put("/swagger*", "anon");
 		
 		// 配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
 		filterChainDefinitionMap.put("/logout", "logout");
